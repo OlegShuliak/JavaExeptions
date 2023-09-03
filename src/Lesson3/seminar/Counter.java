bad165df54c7645bd31d91364bee999b59188f6c
Oleg  Shuliak <nformals@gmail.com>
2023-09-03 21:22:31 +0300

    Add seminar tasks for lesson 3

---
+++ src/Lesson3/seminar/Counter.java
@@ -0,0 +1,34 @@
+package Lesson3.seminar;
+
+import java.io.Closeable;
+
+public class Counter implements AutoCloseable {
+    private int count;
+    private boolean status;
+
+    public Counter(){
+        this.count = 0;
+        this.status = true;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void add() throws ObjectNotAvailable{
+        if(!this.status){
+            throw new ObjectNotAvailable("Объект недоступен.");
+        }
+        this.count++;
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.count = 0;
+        this.status = false;
+    }
+}
---
+++ src/Lesson3/seminar/ObjectNotAvailable.java
@@ -0,0 +1,9 @@
+package Lesson3.seminar;
+
+import java.io.IOException;
+
+public class ObjectNotAvailable extends IOException {
+    public ObjectNotAvailable(String msg){
+        super(msg);
+    }
+}
